33306c131e1984ca77f66db91256d1167c31e3ad
pvande <pvande@gmail.com>
2009-07-26 23:18:06 -0700

    Improvements

--- mini/unit.pl
+++ mini/unit.pl
@@ -35,33 +35,41 @@ class Mini::Unit::TestCase with Mini::Unit::Assertions {
   method teardown() {}
 
   method run($runner) {
-    my $result;
+    my $test = $self->name();
 
     try {
-      my $test = $self->name();
       $self->setup();
       $self->$test();
       $self->passed(1);
-    } catch ($e) {
+    }
+    catch (Mini::Unit::Skip $e) {
+      $self->passed(0);
+      $runner->skip(ref $self, $test, $e->message());
+    }
+    catch (Mini::Unit::Assert $e) {
+      $self->passed(0);
+      $runner->fail(ref $self, $test, $e->message());
+    }
+    catch ($e) {
       # TODO: Ignore reasonable exceptions.
       $self->passed(0);
-      $runner->puke(__PACKAGE__, $self->name(), $e);
+      $runner->error(ref $self, $test, $e);
     };
 
     try {
       $self->teardown();
       $runner->pass(__PACKAGE__, $self->name()) if $self->passed();
-    } catch ($e) {
+    }
+    catch ($e) {
       # TODO: Ignore reasonable exceptions.
-      $runner->puke(__PACKAGE__, $self->name(), $e);
+      $runner->error(ref $self, $test, $e);
     };
   }
 }
 
 role Mini::Unit::Logger {
-  has verbose     => (is => 'ro');
-  has _begin_time => (is => 'rw');
-  has _start_time => (is => 'rw');
+  has _times  => (is => 'ro', default => sub { { starts => {}, ends => {} } });
+  has verbose => (is => 'ro');
 
   method print(@msg) {
     print STDOUT @msg;
@@ -71,49 +79,90 @@ role Mini::Unit::Logger {
     print STDOUT (join("\n", @msg), "\n")
   }
 
-  method begin() {
-    $self->puts("Loaded Suite\n");
-    $self->_begin_time(time());
+
+  method _start(@keys) {
+    $self->_times()->{starts}->{join '#', @keys} = time();
   }
 
-  method start_test(ClassName $suite, Str $test) {
-    $self->print("$suite#$test: ") if $self->verbose();
-    $self->_start_time(time());
+  method _stop(@keys) {
+    $self->_times()->{ends}->{join '#', @keys} = time();
   }
 
-  method pass(ClassName $suite, Str $test) {
+  method time_for(@keys) {
+    my $times = $self->_times();
+    my $key = join '#', @keys;
+    my $start = $times->{starts}->{$key};
+    my $end   = $times->{ends}->{$key} || time();
+    return $end - $start;
+  }
+
+  method total_time() { $self->time_for('__SUITE__') }
+
+
+  method begin_test_suite($filter?) {
+    $self->_start('__SUITE__');
+  }
+
+  method begin_test_case(ClassName $tc, @tests) {
+    $self->_start($tc);
+  }
+
+  method begin_test(ClassName $tc, Str $test) {
+    $self->_start($tc, $test);
+  }
+
+  method finish_test(ClassName $tc, Str $test) {
+    $self->_stop($tc, $test);
+  }
+
+  method finish_test_case(ClassName $tc, @tests) {
+    $self->_stop($tc);
+  }
+
+  method finish_test_suite($filter?) {
+    $self->_stop('__SUITE__');
+  }
+
+
+  method pass(ClassName $tc, Str $test)            { }
+  method fail(ClassName $tc, Str $test, Str $msg)  { }
+  method skip(ClassName $tc, Str $test, Str $msg)  { }
+  method error(ClassName $tc, Str $test, Str $msg) { }
+}
+
+class Mini::Unit::Logger::Default {
+  # TODO: Fix 'with' to work inside a class scope.
+  Moose::with(__PACKAGE__, 'Mini::Unit::Logger');
+
+  after begin_test_suite($filter?) {
+    $self->print('Loaded Suite');
+    $self->print(" (Filtered to /$filter/)") if $filter;
+    $self->puts("\n")
+  }
+
+  after finish_test_suite($filter?) {
+    $self->puts("\n", "Finished in @{[$self->total_time()]} seconds.");
+  }
+
+  after pass(ClassName $tc, Str $test) {
     my $result = $self->verbose() ? 'Passed!' : '.';
     $self->print($result);
   }
 
-  method fail(ClassName $suite, Str $test, Str $msg) {
+  after fail(ClassName $tc, Str $test, Str $msg) {
     my $result = $self->verbose() ? "Failed - $msg!" : 'F';
     $self->print($result);
   }
 
-  method skip(ClassName $suite, Str $test, Str $msg) {
+  after skip(ClassName $tc, Str $test, Str $msg) {
     my $result = $self->verbose() ? "Skipped - $msg!" : 'S';
     $self->print($result);
   }
 
-  method error(ClassName $suite, Str $test, Str $msg) {
+  after error(ClassName $tc, Str $test, Str $msg) {
     my $result = $self->verbose() ? "ERROR - $msg" : 'E';
     $self->print($result);
   }
-
-  method end_test(ClassName $suite, Str $test) {
-    $self->puts() if $self->verbose();
-  }
-
-  method finish() {
-    my $time = time() - $self->_begin_time();
-    $self->puts();
-    $self->puts("Finished in $time seconds.");
-  }
-}
-
-class Mini::Unit::Logger::Default with Mini::Unit::Logger {
-
 }
 
 class Mini::Unit {
@@ -124,14 +173,16 @@ class Mini::Unit {
     is => 'rw',
     does => 'Mini::Unit::Logger',
     handles => [
-      begin      => 'begin',
-      start_test => 'start_test',
-      pass       => 'pass',
-      skip       => 'skip',
-      fail       => 'fail',
-      error      => 'error',
-      end_test   => 'end_test',
-      finish     => 'finish,'
+      (map {
+        (
+          "begin_$_"  => "begin_$_",
+          "finish_$_" => "finish_$_",
+        )
+      } qw/ test_suite test_case test /),
+      pass  => 'pass',
+      skip  => 'skip',
+      fail  => 'fail',
+      error => 'error',
     ],
   );
   class_has file => (
@@ -146,55 +197,51 @@ class Mini::Unit {
     }
   }
 
-  method puke(ClassName $suite, Str $test, $err) {
-    try {
-      die $err;
-    } catch (Mini::Unit::Skip $e) {
-      $self->skip($suite, $test, $e->message());
-    } catch (Mini::Unit::Assert $e) {
-      $self->fail($suite, $test, $e->message());
-    } catch {
-      chomp($err);
-      $self->error($suite, $test, $err);
-    }
-  }
-
   method run(@args) {
     my $verbosity = grep { /-v+/ } @args;
     $self->logger(Mini::Unit::Logger::Default->new(verbose => $verbosity));
 
-    $self->begin();
-    $self->run_test_suites();
-    $self->finish();
+    $self->run_test_suite();
 
     return 0;
   }
 
-  method run_test_suites($filter?) {
-    for my $suite (Mini::Unit::TestCase->meta()->subclasses()) {
-      my @tests = grep { /^test/ } $suite->meta()->get_all_method_names();
-      @tests = $self->sort_tests(@tests);
-      for my $test (grep { $filter || /./ } @tests) {
-        my $instance = $suite->new(name => $test);
-
-        $self->logger()->start_test($suite, $test);
-        $instance->run($self);
-        $self->logger()->end_test($suite, $test);
-      }
+  method run_test_suite($filter?) {
+    for my $tc (Mini::Unit::TestCase->meta()->subclasses()) {
+      my @tests = grep { /^test/ } $tc->meta()->get_all_method_names();
+      $self->run_test_case($tc, grep { $filter || /./ } @tests);
     }
+  }
+
+  method run_test_case(ClassName $tc, @tests) {
+    $self->run_test($tc, $_) for $self->sort_tests(@tests);
+  }
+
+  method run_test(ClassName $tc, Str $test) {
+    $tc->new(name => $test)->run($self)
   }
 
   method sort_tests(@tests) {
     # TODO: Allow tests to be randomly ordered
     @tests
   }
+
+
+  before run_test_suite($filter?) { $self->begin_test_suite($filter);  }
+  after  run_test_suite($filter?) { $self->finish_test_suite($filter); }
+
+  before run_test_case($tc, @tests) { $self->begin_test_case($tc, @tests);  }
+  after  run_test_case($tc, @tests) { $self->finish_test_case($tc, @tests); }
+
+  before run_test($tc, $test) { $self->begin_test($tc, $test);  }
+  after  run_test($tc, $test) { $self->finish_test($tc, $test); }
 }
 
 Mini::Unit->autorun();
 
 if ($0 eq __FILE__) {
 
-  class TestSuite extends Mini::Unit::TestCase {
+  class TestCase extends Mini::Unit::TestCase {
     method test_pass {}
     method test_fail { $self->assert(0, 'Failed HARD!') }
     method test_skipped { Mini::Unit::Skip->throw('Skipped!') }
