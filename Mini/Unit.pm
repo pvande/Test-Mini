6a05bb255146e562b941a9fb46e593d5caa0fb4e
pvande <pvande@gmail.com>
2009-07-27 22:29:33 -0700

    Exit Codes

--- Mini/Unit.pm
+++ Mini/Unit.pm
@@ -20,12 +20,16 @@ class Mini::Unit {
       error => 'error',
     ],
   );
+
+  has exit_code => ( is => 'rw', default => 1 );
+
   # class_has file => (
   #   is      => 'ro',
   #   default => sub { use Cwd 'abs_path'; abs_path(__FILE__); },
   # );
 
-  method autorun(ClassName $class:) {
+  method autorun(ClassName $class:)
+  {
     END {
       $| = 1;
       return if $?;
@@ -33,31 +37,34 @@ class Mini::Unit {
     }
   }
 
-  method run(@args) {
+  method run(@args)
+  {
     my $verbosity = grep { /-v+/ } @args;
     $self->logger(Mini::Unit::Logger::XUnit->new(verbose => $verbosity));
-
     $self->run_test_suite();
-
-    return 0;
+    return $self->exit_code();
   }
 
-  method run_test_suite($filter?) {
+  method run_test_suite($filter?)
+  {
     for my $tc (Mini::Unit::TestCase->meta()->subclasses()) {
       my @tests = grep { /^test/ } $tc->meta()->get_all_method_names();
       $self->run_test_case($tc, grep { $filter || /./ } @tests);
     }
   }
 
-  method run_test_case(ClassName $tc, @tests) {
+  method run_test_case(ClassName $tc, @tests)
+  {
     $self->run_test($tc, $_) for $self->sort_tests(@tests);
   }
 
-  method run_test(ClassName $tc, Str $test) {
+  method run_test(ClassName $tc, Str $test)
+  {
     $tc->new(name => $test)->run($self)
   }
 
-  method sort_tests(@tests) {
+  method sort_tests(@tests)
+  {
     # TODO: Allow tests to be randomly ordered
     @tests
   }
@@ -71,6 +78,10 @@ class Mini::Unit {
 
   before run_test($tc, $test) { $self->begin_test($tc, $test);  }
   after  run_test($tc, $test) { $self->finish_test($tc, $test); }
+
+  before run_test($tc, $test)    { $self->exit_code(0) }
+  after  fail($tc, $test, $msg)  { $self->exit_code(1) }
+  after  error($tc, $test, $msg) { $self->exit_code(1) }
 }
 
 Mini::Unit->autorun();
