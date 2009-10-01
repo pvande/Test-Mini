9e2553421e33af193cb89b4020ddb6e06cb05c9d
pvande <pvande@gmail.com>
2009-09-30 19:34:51 -0700

    Introducing some basic tests for the TestRB Logger.

--- lib/MiniTest/Unit/Logger/TestRB.pm
+++ lib/MiniTest/Unit/Logger/TestRB.pm
@@ -1,6 +1,6 @@
 use MooseX::Declare;
 
-class MiniTest::Unit::Logger::XUnit is dirty
+class MiniTest::Unit::Logger::TestRB is dirty
 {
   with qw/
     MiniTest::Unit::Logger
@@ -41,88 +41,88 @@ class MiniTest::Unit::Logger::XUnit is dirty
     },
   );
 
-  method begin_test_suite(:$filter, :$seed)
+  method begin_test_suite(:$filter, :$seed!)
   {
     $self->print('Loaded Suite');
     $self->print(" (Filtered to /$filter/)") if $filter;
     $self->say("\nSeeded with $seed\n");
   }
 
-  method begin_test(ClassName $tc, Str $test)
-  {
-    $self->print("$tc#$test: ") if $self->verbose();
-  }
-
-  method finish_test(ClassName $tc, Str $test, @)
-  {
-    $self->print("@{[ $self->time_for($tc, $test) ]} s: ") if $self->verbose();
-    $self->print($self->result() || ());
-    $self->say() if $self->verbose();
-  }
-
-  method finish_test_suite(@)
-  {
-    $self->say() unless $self->verbose();
-    $self->say('', "Finished in @{[$self->total_time()]} seconds.");
-
-    my $i = 1;
-    $self->say(sprintf("\n%3d) %s", $i++, $_)) for @{ $self->report() };
-
-    $self->say();
-    $self->say($self->statistics());
-  }
-
-
-  method pass(ClassName $tc, Str $test)
-  {
-    $self->result('.');
-  }
-
-  method fail(ClassName $tc, Str $test, $e)
-  {
-    $self->result('F');
-    $self->add_to_report(
-      sprintf(
-        "Failure:\n%s(%s) [%s]:\n%s",
-        $test,
-        $tc,
-        location($e),
-        $e->message,
-      )
-    );
-  }
-
-  method skip(ClassName $tc, Str $test, $e)
-  {
-    $self->result('S');
-    $self->add_to_report(
-      sprintf(
-        "Skipped:\n%s(%s) [%s]:\n%s",
-        $test,
-        $tc,
-        location($e),
-        $e->message,
-      )
-    );
-  }
-
-  method error(ClassName $tc, Str $test, $e)
-  {
-    my $msg = $e;
-    if (ref $e) {
-      my @trace = clean_backtrace($e);
-      @trace = map { '  ' . $_->as_string } @trace; # TODO: Use friendlier @_ dump
-      $msg = $e->message . join "\n", @trace;
-    }
-
-    $self->result('E');
-    $self->add_to_report(
-      sprintf(
-        "Error:\n%s(%s):\n%s",
-        $test,
-        $tc,
-        $msg,
-      )
-    );
-  }
+  # method begin_test(ClassName $tc, Str $test)
+  # {
+  #   $self->print("$tc#$test: ") if $self->verbose();
+  # }
+  # 
+  # method finish_test(ClassName $tc, Str $test, @)
+  # {
+  #   $self->print("@{[ $self->time_for($tc, $test) ]} s: ") if $self->verbose();
+  #   $self->print($self->result() || ());
+  #   $self->say() if $self->verbose();
+  # }
+  # 
+  # method finish_test_suite(@)
+  # {
+  #   $self->say() unless $self->verbose();
+  #   $self->say('', "Finished in @{[$self->total_time()]} seconds.");
+  # 
+  #   my $i = 1;
+  #   $self->say(sprintf("\n%3d) %s", $i++, $_)) for @{ $self->report() };
+  # 
+  #   $self->say();
+  #   $self->say($self->statistics());
+  # }
+  # 
+  # 
+  # method pass(ClassName $tc, Str $test)
+  # {
+  #   $self->result('.');
+  # }
+  # 
+  # method fail(ClassName $tc, Str $test, $e)
+  # {
+  #   $self->result('F');
+  #   $self->add_to_report(
+  #     sprintf(
+  #       "Failure:\n%s(%s) [%s]:\n%s",
+  #       $test,
+  #       $tc,
+  #       location($e),
+  #       $e->message,
+  #     )
+  #   );
+  # }
+  # 
+  # method skip(ClassName $tc, Str $test, $e)
+  # {
+  #   $self->result('S');
+  #   $self->add_to_report(
+  #     sprintf(
+  #       "Skipped:\n%s(%s) [%s]:\n%s",
+  #       $test,
+  #       $tc,
+  #       location($e),
+  #       $e->message,
+  #     )
+  #   );
+  # }
+  # 
+  # method error(ClassName $tc, Str $test, $e)
+  # {
+  #   my $msg = $e;
+  #   if (ref $e) {
+  #     my @trace = clean_backtrace($e);
+  #     @trace = map { '  ' . $_->as_string } @trace; # TODO: Use friendlier @_ dump
+  #     $msg = $e->message . join "\n", @trace;
+  #   }
+  # 
+  #   $self->result('E');
+  #   $self->add_to_report(
+  #     sprintf(
+  #       "Error:\n%s(%s):\n%s",
+  #       $test,
+  #       $tc,
+  #       $msg,
+  #     )
+  #   );
+  # }
 }
---
+++ t/MiniTest/Unit/Logger/TestRB.t
@@ -0,0 +1,45 @@
+use MiniTest::Unit;
+
+class MyClass { }
+
+testcase MiniTest::Unit::Logger::TestRB::Test
+{
+  use IO::Scalar;
+  use aliased 'MiniTest::Unit::Logger::TestRB' => 'TestRBLogger';
+
+  has 'logger' => (is => 'rw');
+  has 'buffer' => (is => 'rw');
+
+  method setup
+  {
+    $self->logger(
+      TestRBLogger->new(
+        runner => undef,
+        buffer => my $buffer = IO::Scalar->new(),
+      )
+    );
+    $self->buffer($buffer);
+  }
+
+  test begin_test_suite_without_filter
+  {
+    $self->logger->begin_test_suite(seed => 'SEED');
+    assert_equal <<TestRB, "@{[$self->buffer]}"
+Loaded Suite
+Seeded with SEED
+
+TestRB
+  }
+
+  test begin_test_suite_with_filter
+  {
+    $self->logger->begin_test_suite(seed => 'SEED', filter => 'FILTER');
+    assert_equal <<TestRB, "@{[$self->buffer]}"
+Loaded Suite (Filtered to /FILTER/)
+Seeded with SEED
+
+TestRB
+  }
+
+  # TODO: Finish Tests
+}
