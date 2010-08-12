9e7343246edf71d886497108f8148c4d7c24308f
Pieter van de Bruggen <pvande@gmail.com>
2010-08-12 16:38:56 -0700

    Cleaned up the `test` keyword.

--- lib/Test/Mini/Unit/Sugar/Test.pm
+++ lib/Test/Mini/Unit/Sugar/Test.pm
@@ -1,20 +1,39 @@
-use MooseX::Declare;
+package Test::Mini::Unit::Sugar::Test;
+use base 'Devel::Declare::MethodInstaller::Simple';
+use strict;
+use warnings;
 
-class Test::Mini::Unit::Sugar::Test extends MooseX::Declare::Syntax::Keyword::Method
-{
-  around parse($ctx)
-  {
-    my $offset = $ctx->offset;
-
-    $ctx->skip_declarator();
-    $ctx->skipspace();
-
-    my $linestr = $ctx->get_linestr(); # <identifier> foo_bar { ... }
-    substr($linestr, $ctx->offset(), 0, 'test_');
-    $ctx->set_linestr($linestr); # <identifier> test_foo_bar { ... }
-
-    $ctx->_dd_context->{Offset} = $offset;
-
-    $self->$orig($ctx);
-  }
+sub new {
+    my ($class, %args) = @_;
+    return bless \%args, (ref $class || $class);
 }
+
+sub setup_for {
+    my ($self, $package, %options) = @_;
+    $self->install_methodhandler(
+        into => $package,
+        name => $self->{identifier},
+        %options,
+    );
+}
+
+sub parser {
+    my $self = shift;
+    $self->init(@_);
+
+    $self->skip_declarator;
+    my $name   = $self->strip_name;
+    my $inject = 'my $self = shift;';
+
+    if (defined $name) {
+        $inject = $self->scope_injector_call() . $inject;
+    }
+    else {
+        $name = 0+$self;
+    }
+
+    $self->inject_if_block($inject);
+    $self->install("test_$name");
+}
+
+1;
